834ffbfd90e02a01b02b7897867c85a839bb1824
Cristian Klein <cristian.klein@elastisys.com>
2020-12-10 11:39:27 +0100

    Started app/helm

---
+++ 10-apply-compliantkubernetes-apps.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+: ${CLUSTER_NAME:?"Please set CLUSTER_NAME"}
+
+helm template \
+    apps \
+    apps \
+| kubectl \
+    apply \
+    -f - \
+    --dry-run=server \
+    --context ${CLUSTER_NAME}
+
+helm \
+    --kube-context ${CLUSTER_NAME} \
+    --namespace kube-system \
+    upgrade \
+    --install \
+    apps \
+    apps
