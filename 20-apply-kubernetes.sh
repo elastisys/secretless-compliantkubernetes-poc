b5d320166485d9887260d05e2b5b4a42c787b83d
Cristian Klein <cristian.klein@elastisys.com>
2020-12-10 19:54:29 +0100

    Disable cloud-provider-related tasks in the first step

--- 20-apply-kubernetes.sh
+++ 20-apply-kubernetes.sh
@@ -4,4 +4,9 @@ set -e
 
 : ${CLUSTER_NAME:?"Please set CLUSTER_NAME"}
 
-ansible-playbook -i inventory/$CLUSTER_NAME/hosts --become --become-user=root kubespray/cluster.yml
+ansible-playbook \
+    -i inventory/$CLUSTER_NAME/hosts \
+    --become \
+    --become-user=root \
+    --skip-tags=cloud-provider \
+    kubespray/cluster.yml
