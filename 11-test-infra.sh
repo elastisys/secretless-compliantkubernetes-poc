47c3ebfd57216841cff20d797bf2836dc56d7c5b
Cristian Klein <cristian.klein@elastisys.com>
2020-12-11 08:03:59 +0100

    Add note on host key checking

--- 11-test-infra.sh
+++ 11-test-infra.sh
@@ -4,4 +4,10 @@ set -e
 
 : ${CLUSTER_NAME:?"Please set CLUSTER_NAME"}
 
+if [ -z "$ANSIBLE_HOST_KEY_CHECKING" ]; then
+    echo "NOTE: Consider running this script as:" 2>&1
+    echo "    ANSIBLE_HOST_KEY_CHECKING=False $0" 2>&1
+    echo "" 2>&1
+fi
+
 ansible -i inventory/$CLUSTER_NAME/hosts -m ping all
