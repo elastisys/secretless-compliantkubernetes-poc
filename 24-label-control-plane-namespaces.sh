c2f04dcb60b9721ea80960060b7acfdfbf89c1cb
Cristian Klein <cristian.klein@elastisys.com>
2020-12-14 17:00:16 +0100

    Label control-plane namespaces

---
+++ 24-label-control-plane-namespaces.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+
+: ${CLUSTER_NAME:?"Please set CLUSTER_NAME"}
+
+# Avoid validating and admission webhooks from
+# operating in the kube-system namespace. The Kubernetes cluster may work for a
+# while, then system Pods won't restart unexpectedly, e.g., when masters are
+# rebooted.
+#
+# https://kubernetes.io/docs/reference/access-authn-authz/extensible-admission-controllers/#avoiding-operating-on-the-kube-system-namespace
+
+kubectl label namespace kube-system control-plane="true" --overwrite=true
